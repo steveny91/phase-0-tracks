f0da58b62e14a0d75e79b80fcc71364eeba0f6ea
Ka Yuen <steven_yuen05@hotmail.com>
2017-05-22 11:22:22 -0400

    code to print and track input history and results

--- ruby/Ka_S_Yuen/calculator.rb
+++ ruby/Ka_S_Yuen/calculator.rb
@@ -12,26 +12,29 @@ def calculate(num1,operator, num2)
 end
 
 def calculatorloop
+  history = []
   quit = false
   until quit == true 
-  puts "what would you want to calculate?"
+  puts "what would you want to calculate? Type done to exit"
   input = gets.chomp
   calculate = input.split(' ')
     if input == "done"
       quit = true
-      return "goodbye"
     else
       num1 = calculate[0].to_i
       num2 = calculate[2].to_i
       operator = calculate[1]
-      p calculate(num1,operator,num2)
+      result = calculate(num1,operator,num2)
+      puts result
+      history << "#{num1} #{operator} #{num2} = #{result}"
     end
   end
+  numdone =  history.length 
+  puts "#{numdone} calculations completed"
+  puts history
 end
 
 calculatorloop
-
-
 =begin
 puts calculate(4, '+', 5)
 puts calculate(4, '-', 5)
