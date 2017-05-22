05e95f72b9137bd84fcbc98bc3717192e48d5051
Ka Yuen <steven_yuen05@hotmail.com>
2017-05-22 11:09:54 -0400

    working loop for continuous user input

--- ruby/Ka_S_Yuen/calculator.rb
+++ ruby/Ka_S_Yuen/calculator.rb
@@ -11,13 +11,25 @@ def calculate(num1,operator, num2)
   
 end
 
-puts "what would you want to calculate?"
-calculate = gets.chomp.split(' ')
-num1 = calculate[0].to_i
-num2 = calculate[2].to_i
-operator = calculate[1]
+def calculatorloop
+  quit = false
+  until quit == true 
+  puts "what would you want to calculate?"
+  input = gets.chomp
+  calculate = input.split(' ')
+    if input == "done"
+      quit = true
+      return "goodbye"
+    else
+      num1 = calculate[0].to_i
+      num2 = calculate[2].to_i
+      operator = calculate[1]
+      p calculate(num1,operator,num2)
+    end
+  end
+end
 
-calculate(num1,operator,num2)
+calculatorloop
 
 
 =begin
