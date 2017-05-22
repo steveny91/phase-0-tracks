735fd2cd279acdaa781ffed97f7e11799e32036e
Ka Yuen <steven_yuen05@hotmail.com>
2017-05-22 10:56:47 -0400

    working user interface to calculate

--- ruby/Ka_S_Yuen/calculator.rb
+++ ruby/Ka_S_Yuen/calculator.rb
@@ -11,7 +11,18 @@ def calculate(num1,operator, num2)
   
 end
 
+puts "what would you want to calculate?"
+calculate = gets.chomp.split(' ')
+num1 = calculate[0].to_i
+num2 = calculate[2].to_i
+operator = calculate[1]
+
+calculate(num1,operator,num2)
+
+
+=begin
 puts calculate(4, '+', 5)
 puts calculate(4, '-', 5)
 puts calculate(4, '*', 5)
 puts calculate(4, '/', 5)
+=end
