f6fd5dcd4fb1b3becc0768aa14a914022c059819
Ka Yuen <steven_yuen05@hotmail.com>
2017-05-24 19:00:05 -0400

    simple class puppy design

---
+++ ruby/puppy.rb
@@ -0,0 +1,37 @@
+=begin
+Dog = class
+Breed ------------------------
+Labrador
+Retriever
+Husky
+German Sheppard
+Malamute
+
+
+CHARACTERISTICS ----------------
+Gender: Varies
+Friendly: Varies
+Eye count: 2
+Name: varies
+Sense of smell: yes
+
+
+COLOR ----------------
+White
+Black
+Grey
+Brown
+
+BEHAVIOR -----------------------
+Barking
+Eating
+Sleeping
+Playing
+Chewing
+
+SIZE -----------------------
+Small
+Medium
+Big 
+HUGE
+=end
