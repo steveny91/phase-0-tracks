f6797fa627a95381a05b99d76d45f257ae31be2d
Ka Yuen <steven_yuen05@hotmail.com>
2017-05-24 18:49:09 -0400

    puppy learned how to roll over and we can estimate age

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -11,9 +11,18 @@ class Puppy
     }
   end
 
+  def roll_over 
+    puts "Roll over"
+  end
+
+  def dog_years(age)
+    puts age * 7 
+  end
 
 end
 
 fido = Puppy.new
 fido.fetch("ball")
 fido.speak(5)
+fido.roll_over
+fido.dog_years(6)
