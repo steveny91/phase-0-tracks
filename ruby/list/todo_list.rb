534eee71fe5ac313ac6c3de3bbac56a8cd60652b
Ka Yuen <steven_yuen05@hotmail.com>
2017-05-27 10:50:29 -0400

    working calculator spec and todo spec. somework done on santa.

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,24 @@
+class TodoList
+
+  def initialize(list)
+    @list = list
+  end
+
+  def get_items
+    @list
+  end
+
+  def add_item(new_item)
+    @list << new_item
+  end
+
+  def delete_item(done)
+    @list.delete(done)
+  end
+
+  def get_item(integer)
+    @list[integer]
+  end
+
+end
+
