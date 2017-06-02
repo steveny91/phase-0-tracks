09fb79ce12891614159d66a7370a268a778de3eb
Ka Yuen <steven_yuen05@hotmail.com>
2017-06-01 22:10:46 -0400

    psuedocode for the reverse method of javascript added

---
+++ js/explore.js
@@ -0,0 +1,13 @@
+/*
+Psuedocode:
+Method that takes in a string
+Input = string
+Output = string
+  Take a string
+  Take last letter of string and put it at the beginning of a new string(reverse string)
+  Continue adding the last letter of string to the new string until there is no more letters in the input string
+  Give the reverse string if conditions are met
+  Else just give the original string
+*/
+
+
