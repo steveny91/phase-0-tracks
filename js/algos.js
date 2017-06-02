a28cc72e842e07b18a9f09bbbb7633305a300187
Ka Yuen <steven_yuen05@hotmail.com>
2017-06-02 17:45:28 -0400

    pseudocoded the key value pair matching function

--- js/algos.js
+++ js/algos.js
@@ -25,3 +25,16 @@ function longest_word(string){
 }
 
 longest_word(string_array)
+
+
+/*
+Pseudocode
+Function to compare the key:value pairs of each hash to find if they're identical or not
+input : 2 hashes
+output: True or false depending if there's any matches
+  Take two hashes
+  Compare the values of each hash same key
+  If value of the same key is the same then there is a match
+  If the value is not true then proceed to next key and compare
+  If no key value pair matches then return false
+*/
