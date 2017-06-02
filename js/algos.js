40aa2fbe346fc8307d41f1e81f5f95fd5cd8a6fd
Ka Yuen <steven_yuen05@hotmail.com>
2017-06-02 12:09:46 -0400

    pseudocoded the longest phrase function

---
+++ js/algos.js
@@ -0,0 +1,11 @@
+/*
+Pseudocode
+Function to select longest phrase in an array of strings
+input: array of strings
+output: longest phrase in an array
+  Take an array of strings
+  Start with the first word and compare it with the second word
+  The longer word will be kept
+  Then compare the longer word with next word in the array and keep the longer word
+  repeat until whole array has been checked.
+
