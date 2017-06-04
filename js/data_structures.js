4b3fa6443a66d85d7e35f98bd62df7c72e508179
Ka Yuen <steven_yuen05@hotmail.com>
2017-06-04 16:20:51 -0400

    coded a function to create cars based on color, type seat number and one function

--- js/data_structures.js
+++ js/data_structures.js
@@ -13,3 +13,15 @@ for (var i = 0; i < color.length; i++){
 
 console.log(horse_color)
 
+function car_assembly(color, type, seat_number){
+  this.color = color;
+  this.type = type;
+  this.seat_number = seat_number
+  this.ignition = function () {console.log("vroom vroom vroom")};
+  console.log("generating a new "+color+ " " + type + " with "+ seat_number + " seats")
+}
+
+var first_car = new car_assembly("blue", "sedan", 4);
+console.log(first_car)
+first_car.ignition()
+
