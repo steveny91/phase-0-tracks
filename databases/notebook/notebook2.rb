7ac91661a0242280194d70ba062e6c42c9f911fe
Ka Yuen <steven_yuen05@hotmail.com>
2017-06-12 11:24:42 -0400

    removed an extra feature of the notebook. But everything else seems to be working

--- databases/notebook/notebook2.rb
+++ databases/notebook/notebook2.rb
@@ -15,19 +15,8 @@ create_table_cmd_1 = <<-SQL
 SQL
 
 
-#create a table to hold recipes
-create_table_cmd_2 = <<-SQL
-  CREATE TABLE IF NOT EXISTS recipes(
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
-    ingredient varchar(255),
-    body varchar(255)
-  )
-SQL
-
-
 #create a table to hold notes
-create_table_cmd_3 = <<-SQL
+create_table_cmd_2 = <<-SQL
   CREATE TABLE IF NOT EXISTS notes(
     id INTEGER PRIMARY KEY,
     name VARCHAR(255),
@@ -40,16 +29,6 @@ SQL
 #Creation of the tables
 notebook.execute(create_table_cmd_1)
 notebook.execute(create_table_cmd_2)
-notebook.execute(create_table_cmd_3)
-
-
-
-
-
-
-
-
-
 
 
 #methods
@@ -64,7 +43,7 @@ def data_view(notebook, selection)
       puts "#{k[1]}" 
       puts "#{k[2]}"
     end
-  elsif selection == "notes" or selection == "recipes"
+  elsif selection == "notes"
     selected.each do |k|
       puts "#{k['name']}" 
       puts "#{k['body']}"
@@ -83,9 +62,7 @@ def note_add(name, subject, note, notebook)
   notebook.execute("INSERT INTO notes (name, subject, body) values (?,?,?)", [name, subject, note])
 end
 
-def recipe_add(name, ingredient, recipe, notebook)
-  notebook.execute("INSERT INTO recipes (name, ingredient, body) values (?,?,?)", [name, ingredient, recipe])
-end
+
 
 #methods to edit entries
 def phone_edit(name, phone_number, notebook)
@@ -96,9 +73,6 @@ def note_edit(name, subject, note, notebook)
   notebook.execute("update notes set subject=?, body=? where name=?", [subject, note, name])
 end
 
-def recipe_edit(name, ingredient, recipe, notebook)
-  notebook.execute("update recipes set ingredient=?, body=? where name=?", [ingredient, recipe, name])
-end
 
 #methods to delete entries
 def phone_delete(name, notebook)
@@ -109,9 +83,7 @@ def note_delete(name, notebook)
   notebook.execute("delete from notes where name=?", [name])
 end
 
-def recipe_delete(name, notebook)
-  notebook.execute("delete from recipes where name=?", [name])
-end
+
 
 #data deletion
 
@@ -120,8 +92,6 @@ def data_delete(access_table, name, notebook)
     phone_delete(name, notebook)
   elsif access_table == "notes"
     note_delete(name, notebook)
-  elsif access_table == "recipes" 
-    recipe_delete(name, notebook)
   end
 end
 
@@ -157,8 +127,6 @@ phone_add("Steve", 5555555554, notebook)
 phone_add("Stevie", 5555555556, notebook)
 note_add("paragraph tags", "html"," p tags or paragraph tags are inline elements", notebook)
 note_add("paragraph", "html"," p tags or paragraph tags are inline elements", notebook)
-recipe_add("grilled chicken", "chicken", "put chicken on a grill with a little bit of oil. Salt and pepper to taste", notebook)
-recipe_add("grilled chicken", "chicken", "put chicken on a grill with a little bit of oil", notebook)
 
 
 
@@ -172,14 +140,6 @@ recipe_add("grilled chicken", "chicken", "put chicken on a grill with a little b
 
 
 
-
-
-phone_edit("Steven", 6666666666, notebook)
-note_edit("paragraph tags", "html"," <p> tags or paragraph tags are inline elements", notebook)
-recipe_edit("grilled chicken", "chicken breast", "put chicken on a grill with a little bit of oil. Salt and pepper to taste after done cooking", notebook)
-note_delete("paragraph tags", notebook)
-recipe_delete("grilled chicken", notebook)
-phone_delete("Steven", notebook)
 
 
 quit = false
@@ -190,11 +150,11 @@ until quit == true do
   
 
   if perform_option == "view"
-    puts "What book do you want to access? (Notes, Recipes or Phonebook)"
+    puts "What book do you want to access? (Notes or Phonebook)"
     access = gets.chomp.downcase
     data_view(notebook, access)
   elsif perform_option == "add"
-    puts "What book do you want to access? (Notes, Recipes or Phonebook)"
+    puts "What book do you want to access? (Notes or Phonebook)"
     access = gets.chomp.downcase
     puts "what is the title/name"
     name = gets.chomp.capitalize
@@ -202,10 +162,6 @@ until quit == true do
       puts "what's the subject?"
       main_subject = gets.chomp.capitalize
       note_add(name, main_subject, entry_loop, notebook)
-    elsif access == "recipes"
-      puts "what's the main ingredient?"
-      main_subject = gets.chomp.capitalize
-      recipe_add(name, main_subject, entry_loop, notebook)
     elsif access == "phonebook"
       puts "what's the phone number?"
       main_subject = gets.chomp.capitalize
@@ -214,7 +170,7 @@ until quit == true do
     end
     puts "noted"
   elsif perform_option == "edit"
-    puts "What book do you want to access? (Notes, Recipes or Phonebook)"
+    puts "What book do you want to access? (Notes or Phonebook)"
     access = gets.chomp.downcase
     puts "what is the title/name of the page you want to edit?"
     name = gets.chomp.capitalize
@@ -222,10 +178,7 @@ until quit == true do
       puts "what's the subject?"
       main_subject = gets.chomp.capitalize
       note_edit(name, main_subject, entry_loop, notebook)
-    elsif access == "recipes"
-      puts "what's the main ingredient?"
-      main_subject = gets.chomp.capitalize
-      recipe_edit(name, main_subject, entry_loop, notebook)
+    
     elsif access == "phonebook"
       puts "what's the new phone number?"
       main_subject = gets.chomp.capitalize
@@ -234,8 +187,10 @@ until quit == true do
     end
     puts "noted"
   elsif perform_option == "delete"
-    puts "What book do you want to access? (Notes, Recipes or Phonebook)"
+    puts "What book do you want to access? (Notes or Phonebook)"
     access = gets.chomp.downcase
+    puts "What is the name/title of the page you want to delete?"
+    name = gets.chomp
     data_delete(access, name, notebook)
     puts "deleted"
   elsif perform_option == "quit"
@@ -299,5 +254,52 @@ subject_add("ruby", notebook)
 subject_add("sql", notebook)
 subject_add("html", notebook)
 
-  
+#create a table to hold recipes
+create_table_cmd_2 = <<-SQL
+  CREATE TABLE IF NOT EXISTS recipes(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    ingredient varchar(255),
+    body varchar(255)
+  )
+SQL
+
+def recipe_add(name, ingredient, recipe, notebook)
+  notebook.execute("INSERT INTO recipes (name, ingredient, body) values (?,?,?)", [name, ingredient, recipe])
+end
+def recipe_edit(name, ingredient, recipe, notebook)
+  notebook.execute("update recipes set ingredient=?, body=? where name=?", [ingredient, recipe, name])
+end
+
+def recipe_delete(name, notebook)
+  notebook.execute("delete from recipes where name=?", [name])
+end
+
+
+recipe_add("grilled chicken", "chicken", "put chicken on a grill with a little bit of oil. Salt and pepper to taste", notebook)
+recipe_add("grilled chicken", "chicken", "put chicken on a grill with a little bit of oil", notebook)
+
+recipe_edit("grilled chicken", "chicken breast", "put chicken on a grill with a little bit of oil. Salt and pepper to taste after done cooking", notebook)
+
+recipe_delete("grilled chicken", notebook)
+
+ elsif access == "recipes"
+      puts "what's the main ingredient?"
+      main_subject = gets.chomp.capitalize
+      recipe_add(name, main_subject, entry_loop, notebook)
+elsif access == "recipes"
+      puts "what's the main ingredient?"
+      main_subject = gets.chomp.capitalize
+      recipe_edit(name, main_subject, entry_loop, notebook)
+
+def data_delete(access_table, name, notebook)
+  if access_table == "phonebook"
+    phone_delete(name, notebook)
+  elsif access_table == "notes"
+    note_delete(name, notebook)
+  elsif access_table == "recipes" 
+    recipe_delete(name, notebook)
+  end
+end
+
 =end
