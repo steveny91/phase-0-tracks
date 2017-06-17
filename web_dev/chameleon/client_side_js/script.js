8960c96307e6203a460a0e91aac5053223c6458c
Steven Yuen <Steven_yuen05@hotmail.com>
2017-06-17 01:32:16 -0400

    finished the pairign assingment added a new element to html and create some eventlisteners

---
+++ web_dev/chameleon/client_side_js/script.js
@@ -0,0 +1,20 @@
+
+var elements = document.getElementsByTagName("ul")
+var element = elements[0]
+var list = document.createElement("li");
+var text = document.createTextNode("This a newly inserted text");
+list.appendChild(text);
+element.appendChild(list);
+
+var buttons = document.getElementsByTagName("button");
+var button = buttons[0]
+function button1(){
+  alert( "You clicked on the button");
+}
+
+function button2(){
+  alert ("Don't do it!");
+}
+
+button.addEventListener("click", button1)
+buttons[1].addEventListener("mouseover", button2)
